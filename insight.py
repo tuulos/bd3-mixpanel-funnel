3a89c750abfe3bb3faeeda6918046e83c508eb96
Ville Tuulos <team+foobar@bitdeli.com>
2013-02-13 23:21:04 +0000

    add TextInput

--- insight.py
+++ insight.py
@@ -1,14 +1,19 @@
 from bitdeli.insight import insight
-from bitdeli.widgets import Text, Bar, Table
+from bitdeli.widgets import Bar, Widget
 from discodb.query import Q, Literal, Clause
+
+class TokenInput(Widget):
+    pass
 
 @insight
 def view(model, params):
+    print params
     params = {'events': ['$signup', 'Clip created']}
     def steps(events):
         for i in range(len(events)):
             q = Q([Clause([Literal(event)]) for event in events[:i + 1]])
             yield events[i], len(model.query(q))
-    return [Bar(size=(12, 6),
+    return [TokenInput(size=(12, 1), data={'source': list(model)}),
+            Bar(size=(12, 6),
                 data=list(steps(params['events'])))]
     
