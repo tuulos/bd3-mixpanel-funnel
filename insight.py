8e71ccc97284eefda3e5f16d0570e809d4bce20a
Ville Tuulos <team+foobar@bitdeli.com>
2013-02-14 00:48:56 +0000

    get events from TokenInput

--- insight.py
+++ insight.py
@@ -1,5 +1,5 @@
 from bitdeli.insight import insight
-from bitdeli.widgets import Bar, Widget
+from bitdeli.widgets import Text, Bar, Table, Widget
 from discodb.query import Q, Literal, Clause
 
 class TokenInput(Widget):
@@ -7,13 +7,18 @@ class TokenInput(Widget):
 
 @insight
 def view(model, params):
-    print params
-    params = {'events': ['$signup', 'Clip created']}
+    chosen = params['events']['value'] if 'events' in params else []
     def steps(events):
         for i in range(len(events)):
             q = Q([Clause([Literal(event)]) for event in events[:i + 1]])
             yield events[i], len(model.query(q))
-    return [TokenInput(size=(12, 1), data=list(model)),
-            Bar(size=(12, 6),
-                data=list(steps(params['events'])))]
-    
+    widgets = [TokenInput(id='events',
+                          size=(12, 1),
+                          label='Event Sequence',
+                          value=chosen,
+                          data=list(model))]
+    if chosen:
+        widgets += [Bar(id='funnel',
+                        size=(12, 6),
+                        data=list(steps(chosen)))]
+    return widgets
